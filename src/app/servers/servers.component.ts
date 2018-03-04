f9b0724c8e46fc6b989b0cfd6a7e6d18ba0c0714
nydi <stefan.nydegger@gmail.com>
2018-03-04 11:58:55 +0100

    lecture 32

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,19 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
-  // selector: '[app-servers]',
-  // selector: '.app-servers',
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer: boolean = false;
+  serverCreationStatus: string = 'no server was created';
+  serverName: string = 'Testserver';
 
-  ngOnInit() {
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 1000);
   }
 
+  onServerCreation() {
+    this.serverCreationStatus = 'server was created, name is ' + this.serverName;
+  }
+
+  ngOnInit() {
+
+  }
+
+  onUpdateServerName(event: Event) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
